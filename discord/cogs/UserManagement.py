28c146efc07ee9033b77cc2c0b9444327a4caeb5
Yuki Watanabe <ukwhatn@gmail.com>
2025-04-30 00:01:23 +0900

    feat: add_role_to_everyone

---
+++ discord/cogs/UserManagement.py
@@ -0,0 +1,36 @@
+import logging
+
+import discord
+from discord.commands import Option, slash_command
+from discord.ext import commands
+
+from db.package.crud import discord_thread_timeline_channels as db_crud
+from db.package.session import get_db
+
+
+class UserManagement(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.logger = logging.getLogger(type(self).__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+    @slash_command(
+        name="add_roles_to_everyone", description="全てのメンバーにロールを一括付与"
+    )
+    @commands.has_permissions(administrator=True)
+    async def add_roles_to_everyone(
+        self,
+        ctx: discord.commands.context.ApplicationContext,
+        role: Option(discord.Role, "ロール", required=True),
+    ):
+        guild = ctx.guild
+        async for member in guild.fetch_members():
+            await member.add_roles(role)
+
+        await ctx.respond(
+            f"{role.name}を全てのメンバーに付与しました。", ephemeral=True
+        )
+
+
+def setup(bot):
+    return bot.add_cog(UserManagement(bot))
